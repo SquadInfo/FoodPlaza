23d8ee5181fe6934809b15be7187ec942b35303a
Squad Infotech <developer.squadinfotech.com>
2019-06-01 13:08:48 +0530

    removed unnecessary code

--- foodplaza/src/androidTest/java/com/example/foodplaza/ExampleInstrumentedTest.java
+++ foodplaza/src/androidTest/java/com/example/foodplaza/ExampleInstrumentedTest.java
@@ -21,6 +21,6 @@ public class ExampleInstrumentedTest {
         // Context of the app under test.
         Context appContext = InstrumentationRegistry.getTargetContext();
 
-        assertEquals("com.example.foodplaza", appContext.getPackageName());
+        assertEquals("com.example.recyclerviewdemo", appContext.getPackageName());
     }
 }
--- pass_data_through_intent/src/main/java/com/example/passdata/constant/APP_CONSTANT.java
+++
@@ -1,6 +0,0 @@
-package com.example.passdata.constant;
-
-public interface APP_CONSTANT
-{
-   String KEY_SAMPLE_DATA="sample_data";
-}
